025e1970c359c2a3a014a6042232997f0d3447ec
Ígor José Rodrigues <igorjr2003@gmail.com>
2026-06-15 09:37:17 -0300

    Configura servidor local do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+const userConfig: UserConfig = {
   plugins: [react(), tailwindcss()],
-})
+}
+
+if (process.env.NODE_ENV === 'development') {
+  userConfig.server = {
+    host: '0.0.0.0',
+    port: 3000,
+  }
+}
+
+export default defineConfig(userConfig)
